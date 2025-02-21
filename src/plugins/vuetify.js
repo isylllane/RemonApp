4a01dbb85d1f370804d36246a334b29fd7b5054e
isylllane <Lutik301@gmail.com>
2025-02-21 16:03:12 +0300

    init

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,20 +14,8 @@ import {createVuetify} from 'vuetify'
 const myCustomLightTheme = {
   dark: false,
   colors: {
-    background: 'var(--tg-theme-bg-color)',
-    surface: '#FFFFFF',
-    'surface-bright': '#FFFFFF',
-    'surface-light': '#EEEEEE',
-    'surface-variant': '#424242',
-    'on-surface-variant': '#EEEEEE',
-    primary: '#1867C0',
-    'primary-darken-1': '#1F5592',
-    secondary: '#48A9A6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
+    background: 'var(--tg-theme-link-color)',
+
   },
   variables: {
     'border-color': '#000000',
