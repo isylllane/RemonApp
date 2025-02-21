b8400e3e77ab446b85ec0d0976b3b4877bc7668e
isylllane <Lutik301@gmail.com>
2025-02-21 15:21:33 +0300

    init

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,24 +1,42 @@
 <script setup>
-const user = window.Telegram.WebApp.initDataUnsafe?.user;
+const user = {
+  "id": 1702859584,
+  "first_name": "keyframes",
+  "last_name": "box",
+  "username": "keyframes_box",
+  "language_code": "ru",
+  "allows_write_to_pm": true,
+  "photo_url": "https://t.me/i/userpic/320/R2eVpsPAiG_R8zsVJmI7p--Clb09aotTTBEVAaBPZH0.svg"
+};
+const photo = user.photo_url;
 </script>
 
 <template>
   <v-container>
     <v-sheet rounded
-             height = '200px'
-             elevation = '2'
-             class = 'mx-auto px-6 py-8'
+             elevation = "2"
+             class = "px-6 py-8 d-flex flex-column justify-center align-center"
     >
-      <v-img src = '`${user.photo_url}`'></v-img>
-      <div class = 'text-h4 mx-auto d-flex justify-center'>Профиль</div>
-      {{ user.first_name }}
-      {{ user.second_name }}
+      <div class = "text-h4 my-2">Профиль</div>
+
+      <v-img
+        :width = "150"
+        aspect-ratio = "1/1"
+        :src = photo
+        class = "rounded-circle"
+      ></v-img>
+      <div class = "py-2">
+        {{ user.first_name }}
+        {{ user.last_name }}
+
+      </div>
+
     </v-sheet>
   </v-container>
 </template>
 
 <style scoped
-       lang = 'sass'
+       lang = "sass"
 >
 
 </style>
