1122a0c5904b8a4fe3c37b74a17c19d271d2f675
isylllane <Lutik301@gmail.com>
2025-02-21 15:25:20 +0300

    init

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,13 +1,15 @@
 <script setup>
-const user = {
-  "id": 1702859584,
-  "first_name": "keyframes",
-  "last_name": "box",
-  "username": "keyframes_box",
-  "language_code": "ru",
-  "allows_write_to_pm": true,
-  "photo_url": "https://t.me/i/userpic/320/R2eVpsPAiG_R8zsVJmI7p--Clb09aotTTBEVAaBPZH0.svg"
-};
+// const user = {
+//   "id": 1702859584,
+//   "first_name": "keyframes",
+//   "last_name": "box",
+//   "username": "keyframes_box",
+//   "language_code": "ru",
+//   "allows_write_to_pm": true,
+//   "photo_url": "https://t.me/i/userpic/320/R2eVpsPAiG_R8zsVJmI7p--Clb09aotTTBEVAaBPZH0.svg"
+// };
+
+const user = window.Telegram.WebApp.initDataUnsafe?.user;
 const photo = user.photo_url;
 </script>
 
